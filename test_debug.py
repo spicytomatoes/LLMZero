3ee131af8240f669981a5d6f4b3c0d2c9b390c63
Carlo Acosta <acostacarlom@gmail.com>
2024-11-05 13:46:12 +0800

    Revert debug to original state

--- test_debug.py
+++ test_debug.py
@@ -3,29 +3,30 @@ from agents.llm_policy import LLMPolicyAgent
 from agents.alfworld_llm_policy import ALFWorldLLMPolicyAgent
 from agents.mcts import MCTSAgent
 from agents.elevator_expert import ElevatorExpertPolicyAgent
-# from environments.ElevatorEnvironment import ElevatorEnvironment
+from environments.ElevatorEnvironment import ElevatorEnvironment
 from environments.BlackjackEnvironment import BlackjackEnvironment
 from environments.ALFWorldEnvironment import ALFWorldEnvironment
 
-# env = ElevatorEnvironment()
+env = ElevatorEnvironment()
 # env = BlackjackEnvironment()
-env = ALFWorldEnvironment()
+# env = ALFWorldEnvironment()
 env_params = {
+            "system_prompt_path": "prompts/prompt_elevator_policy.txt",
             "extract_action_regex": r"optimal action: (.*)",
         }
 # agent = LLMPolicyAgent(env, device="cuda", debug=True, temp=1.0, env_params=env_params)
-agent = ALFWorldLLMPolicyAgent(env, device="cuda", debug=True, temp=1.0, env_params=env_params)
+# agent = ALFWorldLLMPolicyAgent(env, device="cuda", debug=True, temp=1.0, env_params=env_params)
 # agent = ElevatorExpertPolicyAgent()
-# mcts_args = {
-#             "num_simulations": 100,
-#             "c_puct": 500,    #should be proportional to the scale of the rewards 
-#             "gamma": 0.95,
-#             "max_depth": 100,
-#             "num_rollouts": 10,
-#             "backprop_T": 50,
-#         }
+mcts_args = {
+            "num_simulations": 100,
+            "c_puct": 500,    #should be proportional to the scale of the rewards 
+            "gamma": 0.95,
+            "max_depth": 100,
+            "num_rollouts": 10,
+            "backprop_T": 50,
+        }
 # agent = MCTSAgent(env, policy=llm_agent, debug=True, args=mcts_args)
-# agent = MCTSAgent(env, policy=None, debug=True, args=mcts_args)
+agent = MCTSAgent(env, policy=None, debug=True, args=mcts_args)
 
 state, _ = env.reset()
 
