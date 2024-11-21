d5347e6c64d66950ed33fbc02a2707b160e01059
Eric Low <lowshaofu99@gmail.com>
2024-11-22 06:36:42 +0800

    primary fix

--- reproducible.py
+++ reproducible.py
@@ -82,6 +82,11 @@ class AGENT(str, Enum):
         elif env == ENVIRONMENT.ALF_WORLD:
             return [AGENT.LLM_POLICY.value, AGENT.LLM_MCTS.value, AGENT.LLM_ZERO.value, AGENT.MCTS.value]
 
+def log(log_file, text):
+    log_file.write('\n')
+    log_file.write(text)
+    log_file.flush()
+    print(text)
 
 def load_env(env_str: ENVIRONMENT):
     print("env_str", env_str)
@@ -158,15 +163,58 @@ def user_selection():
 
     return selected_env, selected_agent
 
+def run_alfworld(agent_str, agent, env):
+    if os.getenv("ALFWORLD_DATA") is None:
+        raise Exception("Must set path in ALFWORLD_DATA environment variable to use ALFWorld Environment")
+    
+    LOGS_DIR_PATH = 'logs/'
+    FILES_TO_TEST = [
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-AlarmClock-None-Desk-307/trial_T20190907_072303_146844',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-Book-None-Bed-312/trial_T20190908_103648_829231',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-Bowl-None-Fridge-6/trial_T20190906_230933_751794',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-Candle-None-Toilet-413/trial_T20190909_104025_525772',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-CellPhone-None-Bed-322/trial_T20190907_163932_211569',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-CreditCard-None-Shelf-316/trial_T20190909_092853_746076',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-Laptop-None-Bed-302/trial_T20190908_112426_055221',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-Pen-None-SideTable-309/trial_T20190907_051843_166835',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-Pencil-None-SideTable-322/trial_T20190908_112624_358795',
+        '$ALFWORLD_DATA/json_2.1.1/train/pick_and_place_simple-Pillow-None-ArmChair-206/trial_T20190909_011649_821132'
+    ]
+    
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    agent_name = agent_str
 
-def run():
-    selected_env, selected_agent = user_selection()
-
-    env, cfg = load_env(selected_env)
-    agent = load_agent(selected_agent, selected_env, env, cfg)
+    log_file = open(f'{LOGS_DIR_PATH}{agent_name}_trial_logs.log', 'a')
 
     rewards = []
+    num_failure = 0
+    num_success = 0
 
+    for file in FILES_TO_TEST:
+        log(log_file, '--------------------------------------------------')
+
+        env = ALFWorldEnvironment(overwrite_env=file)
+        success, num_steps, total_reward = run_single_trial(env, agent, log_file)
+
+        if success:
+            num_success += 1
+            log(log_file, f'SUCCESS, Total steps: {num_steps}, total reward: {total_reward}')
+        else:
+            num_failure += 1
+            log(log_file, f'FAIL, Total steps: {num_steps}, total reward: {total_reward}')
+    
+        rewards.append(total_reward)
+
+    log(log_file, f"Average reward: {np.mean(rewards)}")
+    log(log_file, f'Num SUCCESS: {num_success}')
+    log(log_file, f'Num FAIL: {num_failure}')
+
+    log_file.close()
+
+def run_elevator(agent_str, agent, env): 
+    rewards = []
+    
     for i in range(1):
         num_steps = 0
         total_reward = 0
@@ -193,6 +241,44 @@ def run():
                 f"Step {num_steps}, action: {env.action_to_text(action)}, reward: {reward}, total reward: {total_reward}")
 
     print("Average reward: ", np.mean(rewards))
+
+def run_single_trial(env, agent, log_file):
+    num_steps = 0
+    total_reward = 0
+    max_num_steps = 40
+
+    state, _ = env.reset()
+    log(log_file, state['text_state'])
+    while True:
+        action = agent.act(state)
+        print(action)
+        next_state, reward, done, _, info = env.step(action)
+
+        num_steps += 1
+        total_reward += reward
+        print(f"Step {num_steps}, action: {env.action_to_text(action)}, reward: {reward}, total reward: {total_reward}")
+        
+        if done:
+            break
+        if num_steps >= max_num_steps:
+            print('Unable to finish task')
+            break
+        
+        state = next_state
+    
+    return done, num_steps, total_reward
+
+
+def run():
+    selected_env, selected_agent = user_selection()
+
+    env, cfg = load_env(selected_env)
+    agent = load_agent(selected_agent, selected_env, env, cfg)
+    
+    if selected_env == ENVIRONMENT.ELEVATOR.name:
+        run_elevator(selected_agent, agent, env)
+    elif selected_env == ENVIRONMENT.ALF_WORLD.name:
+        run_alfworld(selected_agent, agent, env)
 
 
 if __name__ == "__main__":
