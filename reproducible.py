16a471fca0fc9834d281031e4bb6277d11b9eed6
Eric Low <lowshaofu99@gmail.com>
2024-11-22 05:41:03 +0800

    fixing

--- reproducible.py
+++ reproducible.py
@@ -47,6 +47,9 @@ from agents.llmzero import LLMZeroAgent
 from agents.llm_policy import LLMPolicyAgent
 from agents.elevator_expert import ElevatorExpertPolicyAgent
 
+from agents.alfworld_mcts import ALFworldMCTSAgent
+from agents.alfworld_llmzero import ALFWorldLLMZeroAgent
+from agents.alfworld_llm_policy import ALFWorldLLMPolicyAgent
 
 SEED = 42
 np.random.seed(SEED)
@@ -73,8 +76,11 @@ class AGENT(str, Enum):
     LLM_ZERO = "LLM Zero"
 
     @classmethod
-    def list(self):
-        return list(map(lambda c: c.value, self))
+    def list(self, env: ENVIRONMENT):
+        if env == ENVIRONMENT.ELEVATOR:
+            return list(map(lambda c: c.value, self))
+        elif env == ENVIRONMENT.ALF_WORLD:
+            return [AGENT.LLM_POLICY.value, AGENT.LLM_MCTS.value, AGENT.LLM_ZERO.value, AGENT.MCTS.value]
 
 
 def load_env(env_str: ENVIRONMENT):
@@ -82,7 +88,7 @@ def load_env(env_str: ENVIRONMENT):
     if env_str == ENVIRONMENT.ELEVATOR.name:
         env = ElevatorEnvironment()
         cfg = yaml.safe_load(open("configs/elevator.yaml"))
-    elif env_str == "alfworld":
+    elif env_str == ENVIRONMENT.ALF_WORLD.name:
         env = ALFWorldEnvironment()
         cfg = yaml.safe_load(open("configs/alfworld.yaml"))
     else:
@@ -92,9 +98,12 @@ def load_env(env_str: ENVIRONMENT):
 
 def load_agent(agent_str, env: gym.Wrapper, cfg):
     if agent_str == AGENT.LLM_POLICY.name:
-        agent = LLMPolicyAgent(
-            env, device="cuda", debug=False,
-            **cfg["llm_policy"])
+        if env == ENVIRONMENT.ELEVATOR:
+            agent = LLMPolicyAgent(env, device="cuda", debug=False, **cfg["llm_policy"])
+        elif env == ENVIRONMENT.ALF_WORLD:
+            agent = ALFWorldLLMPolicyAgent(env, device="cuda", debug=False, **cfg["llm_policy"])
+        else:
+            raise ValueError("Invalid Environment")
     elif agent_str == AGENT.RANDOM.name:
         agent = RandomAgent(env, seed=SEED)
     elif agent_str == AGENT.MCTS.name:
@@ -106,14 +115,24 @@ def load_agent(agent_str, env: gym.Wrapper, cfg):
         agent = MCTSAgent(env, policy=ElevatorExpertPolicyAgent(),
                           debug=False, args=mcts_args)
     elif agent_str == AGENT.LLM_MCTS.name:
-        llm_agent = LLMPolicyAgent(
-            env, device="cuda", debug=False, **cfg["llm_mcts"]["llm_policy"])
         mcts_args = cfg["llm_mcts"]["mcts_args"]
-        agent = MCTSAgent(env, policy=llm_agent, debug=False, args=mcts_args)
+        if env == ENVIRONMENT.ELEVATOR:
+            llm_agent = LLMPolicyAgent(env, device="cuda", debug=False, **cfg["llm_mcts"]["llm_policy"])
+            agent = MCTSAgent(env, policy=llm_agent, debug=False, args=mcts_args)
+        elif env == ENVIRONMENT.ALF_WORLD:
+            llm_agent = ALFWorldLLMPolicyAgent(env, device="cuda", debug=False, **cfg["llm_mcts"]["llm_policy"])
+            agent = ALFworldMCTSAgent(env, policy=llm_agent, debug=False, args=mcts_args)
+        else:
+            raise ValueError("Invalid Environment")
     elif agent_str == AGENT.NEURAL_NETWORK.name:
         agent = NNAgent(env, cfg=cfg["nn_agent"])
     elif agent_str == AGENT.LLM_ZERO.name:
-        agent = LLMZeroAgent(env)
+        if env == ENVIRONMENT.ELEVATOR:
+            agent = LLMZeroAgent(env)
+        elif env == ENVIRONMENT.ALF_WORLD:
+            agent = ALFWorldLLMZeroAgent(env)
+        else:
+            raise ValueError("Invalid Environment")
     else:
         raise ValueError("Invalid agent")
 
@@ -121,45 +140,23 @@ def load_agent(agent_str, env: gym.Wrapper, cfg):
 
 
 def user_selection():
-    question_list = [
-        inquirer.List("env", "Select an environment",
-                      ENVIRONMENT.list()),
-        inquirer.List("agent", "Select an agent", AGENT.list()),
+    env_question = [
+        inquirer.List("env", "Select an environment", ENVIRONMENT.list()),
     ]
-    answer = inquirer.prompt(question_list)
+    env_answer = inquirer.prompt(env_question)
+    
+    agent_question = [
+        inquirer.List("agent", "Select an agent", AGENT.list(env=ENVIRONMENT(env_answer["env"]))),
+    ]
+    agent_answer = inquirer.prompt(agent_question)
 
-    selected_env = ENVIRONMENT(answer["env"]).name
-    selected_agent = AGENT(answer["agent"]).name
+    selected_env = ENVIRONMENT(env_answer["env"]).name
+    selected_agent = AGENT(agent_answer["agent"]).name
+    
+    # print('ENVIRONMENT(env_answer["env"]).name', ENVIRONMENT(env_answer["env"]).name)
+    # print('AGENT(agent_answer["agent"]).name', AGENT(agent_answer["agent"]).name)
 
     return selected_env, selected_agent
-
-
-def create_env_file_if_needed():
-    env_file = ".env"
-
-    keys = [
-        "OPENAI_API_KEY",
-        "CUSTOM_BASE_URL",
-        "CUSTOM_API_KEY",
-        "CUSTOM_MODEL_ID",
-        "USE_OPENAI_CUSTOM",
-    ]
-    
-    print(f"Checking if {env_file} exists...")
-
-    if not os.path.exists(env_file):
-        print(f"{env_file} not found. Creating it...")
-        open(env_file, 'w').close()
-
-    load_dotenv(env_file)
-
-    for key in keys:
-        if get_key(dotenv_path=env_file, key_to_get=key) is None:
-            print(f"{key} not found in {env_file}. Adding it...")
-            set_key(env_file, key, "")
-            print(f"{key} is added to {env_file}.")
-        else:
-            print(f"{key} already exists in {env_file}. No changes needed.")
 
 
 def run():
